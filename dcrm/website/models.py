2c9c80d248110effff8ecd3c35b9f2f1920cf2e5
Ravi <ravik@technoarchsoftwares.com>
2023-08-01 16:38:07 +0530

    Blog adding and user profile shouldbe shown functionality added

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Record(models.Model):
@@ -14,3 +15,13 @@ class Record(models.Model):
 
 	def __str__(self):
 		return(f"{self.first_name} {self.last_name}")
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    publication_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>', views.update_record, name='update_record'),
+    path('blog/', views.blog_post_list, name='blog_post_list'),
+    path('blog/<int:pk>/', views.blog_post_detail, name='blog_post_detail'),
+    path('blog/add/', views.add_blog_post, name='add_blog_post'),
+    path('author_profile/<int:author_id>/', views.author_profile, name='author_profile'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
 ]
