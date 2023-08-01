5ddd9aa0527fdbb88932ac6f31a2b697385abc9b
Ravi <ravik@technoarchsoftwares.com>
2023-08-01 13:15:28 +0530

    user login and logout templates and functionality added

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-from .views import home
+from . import views
 
 urlpatterns = [
-    path("", home, name='home'),
+    path("", views.home, name='home'),
+    # path("login/", views.login_user, name='login'),
+    path("logout/", views.logout_user, name='logout'),
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, 'You have succesfuly logged in!')
+			return redirect('home')
+		else:
+			messages.success(request, 'There was an error in logged in! Please try again!')
+			return redirect('home')
+
 	return render(request, 'home.html', {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, 'You have been Logged Out.....')
+	return redirect('home')
 
